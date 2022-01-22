f00ec98122ea70375cd43a65dbaa53602e757dbb
Dundar Göc <gocdundar@gmail.com>
2022-01-22 10:56:15 +0100

    fish: add home to format function

--- .config/fish/functions/format.fish
+++ .config/fish/functions/format.fish
@@ -1,6 +1,8 @@
 function format
   set repo_path (git rev-parse --show-toplevel)
-  if [ $repo_path = $HOME/programs/stalker.nvim ]
+  if [ $repo_path = $HOME ]
+    fish_indent --write $XDG_CONFIG_HOME/fish/**/*.fish
+  else if [ $repo_path = $HOME/programs/stalker.nvim ]
     stylua $repo_path
   end
 end
