bdf49a0e97eecc27247fe4a8e32bd3fca13ee35f
Dundar Göc <gocdundar@gmail.com>
2022-01-23 13:36:03 +0100

    fish: create function gh-setup

---
+++ .config/fish/functions/gh-setup.fish
@@ -0,0 +1,7 @@
+# Create gh aliases. This only needs to be done once so ideally it should only
+# be in a setup script instead of a function.
+
+function gh-setup
+    gh alias set create 'pr create --fill'
+    gh extension install dlvhdr/gh-prs
+end
