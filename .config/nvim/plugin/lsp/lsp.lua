531a2b02ae303d4be340f113b7f5342dfd071c34
Dundar Göc <gocdundar@gmail.com>
2022-01-24 20:57:58 +0100

    lsp: use eslint instead of tsserver

--- .config/nvim/plugin/lsp/lsp.lua
+++ .config/nvim/plugin/lsp/lsp.lua
@@ -1,4 +1,4 @@
-local servers = { 'yamlls', 'clangd', 'vimls', 'cmake', 'bashls', 'jsonls', 'tsserver', 'pyright' }
+local servers = { 'yamlls', 'clangd', 'vimls', 'cmake', 'bashls', 'jsonls', 'eslint', 'pyright' }
 for _, server in ipairs(servers) do
     require('lspconfig')[server].setup {
         on_attach = require('lsp').on_attach,
