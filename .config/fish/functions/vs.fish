bd9b49017961afcc7d0683f7e9d7722ef28550e4
Dundar Göc <gocdundar@gmail.com>
2022-01-24 10:55:05 +0100

    fish: create temporary file in /tmp instead of working directory

--- .config/fish/functions/vs.fish
+++ .config/fish/functions/vs.fish
@@ -1,6 +1,7 @@
 function vs
     set filename $argv[-1]
-    vi $argv[1..-2] --startuptime nvim.log $filename
-    vi nvim.log
-    rm -f nvim.log
+    set tmpfile (mktemp)
+    vi $argv[1..-2] --startuptime $tmpfile $filename
+    vi $tmpfile
+    rm -f $tmpfile
 end
