7db112fcf5ffb893fe5c8cd069ed952c2cd9d320
Dundar Göc <gocdundar@gmail.com>
2022-01-29 13:14:17 +0100

    lsp(clangd): set background-index=0 to prevent creating .cache directory

--- .config/nvim/plugin/lsp/lsp.lua
+++ .config/nvim/plugin/lsp/lsp.lua
@@ -1,4 +1,4 @@
-local servers = { 'yamlls', 'clangd', 'vimls', 'cmake', 'bashls', 'jsonls', 'eslint', 'pyright', 'gopls' }
+local servers = { 'yamlls', 'vimls', 'cmake', 'bashls', 'jsonls', 'eslint', 'pyright', 'gopls' }
 for _, server in ipairs(servers) do
     require('lspconfig')[server].setup {
         on_attach = require('lsp').on_attach,
