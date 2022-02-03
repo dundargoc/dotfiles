a731160e770fbeb007d4de04c2d554888c7394df
Dundar Göc <gocdundar@gmail.com>
2022-02-03 17:04:05 +0100

    fish(build-vim): output compile_commands.json to vim folder

--- .config/fish/functions/build-vim.fish
+++ .config/fish/functions/build-vim.fish
@@ -2,7 +2,7 @@ function build-vim
     set vim_path "$HOME/programs/vim"
     touch $vim_path/src/**/*
     if type -q compiledb
-        compiledb make -C $vim_path -j
+        compiledb -o $vim_path/compile_commands.json make -C $vim_path -j
     else
         bear --output $vim_path/compile_commands.json -- make -C $vim_path -j
     end
