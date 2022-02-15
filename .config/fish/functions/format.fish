47317bfccaa81dcdbd871a6325db1e8903ef7ca4
Dundar Göc <gocdundar@gmail.com>
2022-02-15 10:05:03 +0100

    fish(format): add nvim config files to formatting

--- .config/fish/functions/format.fish
+++ .config/fish/functions/format.fish
@@ -2,6 +2,7 @@ function format
     set repo_path (git rev-parse --show-toplevel)
     if [ $repo_path = $HOME ]
         fish_indent --write $XDG_CONFIG_HOME/fish/**/*.fish
+        stylua $HOME/.config/nvim
     else if [ $repo_path = $HOME/programs/stalker.nvim ]
         stylua $repo_path
     end
