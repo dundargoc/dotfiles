b6649de6c2ebaeef9aac0cf9f164567557359aec
Dundar Göc <gocdundar@gmail.com>
2022-02-18 11:24:37 +0100

    fish: create function testlocal

---
+++ .config/fish/functions/testlocal.fish
@@ -0,0 +1,16 @@
+function testlocal
+    set branch "_test-branch"
+    git push origin --delete $branch
+    git branch -D $branch
+
+    git checkout -b $branch
+
+    touch .test-file
+
+    git add -A
+    git commit -m 'test: pull request test'
+
+    createlocal
+
+    git switch (gh-default-branch)
+end
