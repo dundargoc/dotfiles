10c46193378aeacb7ac6acf39fa009c3011f1dd2
Dundar Göc <gocdundar@gmail.com>
2022-02-26 11:12:25 +0100

    nvim(lsp): combine ccls and clangd into a single file

---
+++ .config/nvim/plugin/lsp/c.lua
@@ -0,0 +1,12 @@
+local use_clangd = false
+
+if use_clangd then
+    require('lspconfig')['clangd'].setup {
+        on_attach = require('lsp').on_attach,
+        cmd = { 'clangd', '--background-index=0' },
+    }
+else
+    require('lspconfig')['ccls'].setup {
+        on_attach = require('lsp').on_attach,
+    }
+end
--- .config/nvim/plugin/lsp/clangd.lua
+++
@@ -1,4 +0,0 @@
-require('lspconfig')['clangd'].setup {
-    on_attach = require('lsp').on_attach,
-    cmd = { 'clangd', '--background-index=0' },
-}
