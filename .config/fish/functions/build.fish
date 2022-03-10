dee4faae6ecee52b02214a850219d398729f6a9e
Dundar Göc <gocdundar@gmail.com>
2022-03-10 21:34:45 +0100

    fish(build): use clang as default editor

--- .config/fish/functions/build.fish
+++ .config/fish/functions/build.fish
@@ -1,6 +1,7 @@
 function build
     set neovim_path "$HOME/programs/neovim"
 
+    set -x CC clang
     make -C "$neovim_path" CMAKE_INSTALL_PREFIX="$neovim_path"
 
     command cp $HOME/programs/neovim/build/compile_commands.json $HOME/programs/neovim/
