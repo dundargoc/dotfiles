6e63f650fbf86569fde5aa13ab4dd85763a489b5
Dundar Göc <gocdundar@gmail.com>
2022-03-12 09:41:15 +0100

    fish(cleanubuild): use make distclean instead of manually removing

--- .config/fish/functions/cleanbuild.fish
+++ .config/fish/functions/cleanbuild.fish
@@ -2,8 +2,7 @@ function cleanbuild
     set neovim_path "$HOME/programs/neovim"
 
     reset
-    rm -rf $neovim_path/build
-
+    make -C "$neovim_path" distclean
     make -C "$neovim_path" CMAKE_INSTALL_PREFIX="$neovim_path"
 
     command cp $HOME/programs/neovim/build/compile_commands.json $HOME/programs/neovim/
