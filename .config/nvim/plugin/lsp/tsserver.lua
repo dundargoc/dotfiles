4c85a283f2f4a5d5a0e52dc8b2f7198a16bd248e
Dundar Göc <gocdundar@gmail.com>
2022-03-16 16:06:00 +0100

    lsp(tsserver): disable suggestions from tsserver

---
+++ .config/nvim/plugin/lsp/tsserver.lua
@@ -0,0 +1,8 @@
+require('lspconfig').tsserver.setup {
+    on_attach = require('lsp').on_attach,
+    init_options = {
+        preferences = {
+            disableSuggestions = true,
+        },
+    },
+}
