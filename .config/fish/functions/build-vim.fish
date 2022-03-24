ac25778f192be11248fbf09ed361f347b11682fa
Dundar Göc <gocdundar@gmail.com>
2022-03-24 11:25:46 +0100

    fish(build-vim): append to compile-commands instead of writing on to it

--- .config/fish/functions/build-vim.fish
+++ .config/fish/functions/build-vim.fish
@@ -1,10 +1,9 @@
 function build-vim
     set -x CC clang
     set vim_path "$HOME/programs/vim"
-    touch $vim_path/src/**/*
     if type -q compiledb
         compiledb -o $vim_path/compile_commands.json make -C $vim_path -j
     else
-        bear --output $vim_path/compile_commands.json -- make -C $vim_path -j
+        bear --append --output $vim_path/compile_commands.json -- make -C $vim_path -j
     end
 end
