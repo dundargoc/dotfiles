5b9afa7dea6149cd6ab0834ba5ba725eab08d42a
Dundar Göc <gocdundar@gmail.com>
2022-03-27 16:21:21 +0200

    fish(ss): change ss to sync with my repos instead

--- .config/fish/functions/ss.fish
+++ .config/fish/functions/ss.fish
@@ -1,7 +1,11 @@
 function ss
-    if count $argv >/dev/null
-        gnome-screenshot -d $argv
-    else
-        gnome-screenshot
-    end
+    # Sync Neovim
+    gh repo sync dundargoc/neovim --source neovim/neovim
+    git -C $HOME/programs/neovim fetch --tags
+    git -C $HOME/programs/neovim pull --rebase --all
+
+    # Sync Vim
+    gh repo sync dundargoc/vim --source vim/vim
+    git -C $HOME/programs/vim fetch --tags
+    git -C $HOME/programs/vim pull --rebase --all
 end
