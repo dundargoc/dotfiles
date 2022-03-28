313914c8e376b6347dc6c043c5ab353f770fd46f
Dundar Göc <gocdundar@gmail.com>
2022-03-28 10:04:01 +0200

    fish(cleanbuild): remove build folder instead of running distclean

--- .config/fish/functions/cleanbuild.fish
+++ .config/fish/functions/cleanbuild.fish
@@ -3,7 +3,7 @@ function cleanbuild
     set -x CC clang
 
     reset
-    make -C "$neovim_path" distclean
+    rm -rf "$neovim_path"/build
     make -C "$neovim_path" CMAKE_INSTALL_PREFIX="$neovim_path"
 
     command cp $HOME/programs/neovim/build/compile_commands.json $HOME/programs/neovim/
