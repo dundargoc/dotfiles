9e691171c7fb17b9e2fb185b8cde378262201197
Dundar Göc <gocdundar@gmail.com>
2022-03-30 20:09:14 +0200

    fish(buildviminstall): add distclean

--- .config/fish/functions/buildviminstall.fish
+++ .config/fish/functions/buildviminstall.fish
@@ -2,6 +2,7 @@ function buildviminstall
     set -x CC clang
     set vim_path "$HOME/programs/vim"
 
+    make -C $vim_path distclean
     $vim_path/configure --prefix=$vim_path
     make -C $vim_path -j
     make -C $vim_path install
