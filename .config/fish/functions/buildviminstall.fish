eb56931aceb9820903c6766934ff703654a202c0
Dundar Göc <gocdundar@gmail.com>
2022-04-13 13:40:56 +0200

    fish(buildviminstall): configure and install in same command

--- .config/fish/functions/buildviminstall.fish
+++ .config/fish/functions/buildviminstall.fish
@@ -3,7 +3,5 @@ function buildviminstall
     set vim_path "$HOME/programs/vim"
 
     make -C $vim_path distclean
-    $vim_path/configure --prefix=$vim_path
-    make -C $vim_path -j
-    make -C $vim_path install
+    $vim_path/configure --prefix=$vim_path && make -C $vim_path -j && make -C $vim_path install
 end
