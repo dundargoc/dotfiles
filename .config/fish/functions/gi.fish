ec61ae5bb990544adb1ed3341698e321eab7c194
Dundar Goc <gocdundar@gmail.com>
2022-04-18 20:37:19 +0200

    fish(gi): use cgdb instead of gdb

--- .config/fish/functions/gi.fish
+++ .config/fish/functions/gi.fish
@@ -2,10 +2,10 @@ function gi
     if test -f minimal.vim
         build
         buildinstall
-        gdb -ex=run --args neovim/bin/nvim -S minimal.vim
+        cgdb -ex=run --args neovim/bin/nvim -S minimal.vim
     else if test -f minimal.lua
         build
         buildinstall
-        gdb -ex=run --args neovim/bin/nvim -S minimal.vim
+        cgdb -ex=run --args neovim/bin/nvim -S minimal.vim
     end
 end
