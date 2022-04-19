7d8b0241294a43cb20dabd81bfb18e1e058dd23c
Dundar Goc <gocdundar@gmail.com>
2022-04-19 10:37:04 +0200

    fish(buildinstall): add clang as compiler

--- .config/fish/functions/buildinstall.fish
+++ .config/fish/functions/buildinstall.fish
@@ -1,5 +1,6 @@
 function buildinstall
     set neovim_path "$HOME/programs/neovim"
+    set -x CC clang
 
     make -C "$neovim_path" CMAKE_INSTALL_PREFIX="$neovim_path" CMAKE_BUILD_TYPE=RelWithDebInfo
     make -C "$neovim_path" install
