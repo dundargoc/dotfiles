a6c7b88cc0ef7303e0cdf6c4c205a8220ce2d99d
Dundar Goc <gocdundar@gmail.com>
2022-04-23 15:38:15 +0200

    fish(tidy): specify it's c++11 when using on uncrustify

--- .config/fish/functions/tidy.fish
+++ .config/fish/functions/tidy.fish
@@ -1,6 +1,14 @@
 function tidy
     reset
-    for i in $argv
-        clang-tidy "$i"
+    set repo_path (git rev-parse --show-toplevel)
+
+    if [ $repo_path = $HOME/programs/uncrustify ]
+        for i in $argv
+            clang-tidy "$i" -extra-arg=-std=c++11
+        end
+    else
+        for i in $argv
+            clang-tidy "$i"
+        end
     end
 end
