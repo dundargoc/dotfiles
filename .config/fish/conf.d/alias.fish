55f2d486142438c8a2753370abb59660d063bfb9
Dundar Goc <gocdundar@gmail.com>
2022-04-23 14:54:07 +0200

    fish: add alias cdu

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -137,6 +137,7 @@ alias cdt="cd $PROG/nvim-treesitter"
 alias cdspa="cd $WORK/confighub_spa2/cynosure_product_cli"
 alias cdsv="cd $WORK/services/src/services/victoria/ui"
 alias cdsva="cd $WORK/services/src/services/victoria/ui/ui/components/Activity"
+alias cdu="cd $PROG/uncrustify"
 alias cdv="cd $PROG/vim/src"
 alias cdw="cd $HOME/work"
 
