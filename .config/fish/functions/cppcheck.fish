0f29505a524aebc8a8abd481771894e3c4db9e8d
Dundar Goc <gocdundar@gmail.com>
2022-04-24 12:14:12 +0200

    fish: add function cppcheck

---
+++ .config/fish/functions/cppcheck.fish
@@ -0,0 +1,14 @@
+function cppcheck
+    set repo_path (git rev-parse --show-toplevel)
+    reset
+
+    if [ $repo_path = $HOME/programs/uncrustify ]
+        command cppcheck --project=compile_commands.json --enable=all --quiet -j(nproc) --std=c++11 $argv
+    else if [ $repo_path = $HOME/programs/neovim ]
+        command cppcheck --project=compile_commands.json --enable=all --quiet -j(nproc) --std=c99 $argv
+    else if [ $repo_path = $HOME/programs/vim ]
+        command cppcheck --project=compile_commands.json --enable=all --quiet -j(nproc) --std=c89 $argv
+    else
+        command cppcheck $argv
+    end
+end
