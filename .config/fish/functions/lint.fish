acd223dd2dd52f06fa62babb1fb19596d8994165
Dundar Goc <gocdundar@gmail.com>
2022-04-26 14:09:54 +0200

    fish(lint): modify tox.ini to use any python when linting

--- .config/fish/functions/lint.fish
+++ .config/fish/functions/lint.fish
@@ -3,7 +3,9 @@ function lint
     reset
 
     if [ $repo_path = $HOME/work/confighub_spa2 ]
+        sed 's/python/python3.6/' -i $HOME/work/confighub_spa2/tox.ini
         tox -e pylint
+        git restore $HOME/work/confighub_spa2/tox.ini
     end
 
     if [ $repo_path = $HOME/programs/neovim ]
