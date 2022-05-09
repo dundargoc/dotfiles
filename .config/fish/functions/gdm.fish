467daff88e818cd7a3279d2206e9a08da0bd14e4
Dundar Goc <gocdundar@gmail.com>
2022-05-09 14:14:57 +0200

    fish: find ancestor when checking difference between brances

---
+++ .config/fish/functions/gdm.fish
@@ -0,0 +1,7 @@
+function gdm
+    set default (gh-default-branch)
+    set current (git branch --show-current)
+    set ancestor (git merge-base $default $current)
+
+    git diff $ancestor...$current
+end
