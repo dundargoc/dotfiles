75745d648faf258b8e55961a1bffdb89d2358173
Dundar Goc <gocdundar@gmail.com>
2022-05-12 12:04:52 +0200

    fish(buildviminstall): cd to vim directory before installing

--- .config/fish/functions/buildviminstall.fish
+++ .config/fish/functions/buildviminstall.fish
@@ -2,6 +2,8 @@ function buildviminstall
     set -x CC clang
     set vim_path "$HOME/programs/vim"
 
-    make -C $vim_path distclean
+    pushd $vim_path >/dev/null
+    make distclean
     $vim_path/configure --prefix=$vim_path && make -C $vim_path -j && make -C $vim_path install
+    popd >/dev/null
 end
