3b555cd06578d45a3ab7ae7182ec5534414c0f2c
Dundar Goc <gocdundar@gmail.com>
2022-05-22 21:13:30 +0200

    fish: add alias v

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -283,3 +283,4 @@ alias clean "rm -rf $PROG/neovim/build"
 alias vimclean "find $PROG/neovim -type f -name '*.rej' -delete; find $PROG/neovim -maxdepth 1 -type f -name '*.patch' -delete; make -C $PROG/neovim/src/nvim/testdir clean"
 alias cvim "nvim --clean"
 alias func '$PROG/uncrustify/build/uncrustify -c $PROG/uncrustify/forUncrustifySources.cfg --replace --no-backup $PROG/uncrustify/src/*.{cpp,h}; gres $PROG/uncrustify/src/ChunkStack.h'
+alias v '$HOME/programs/vim/bin/vim'
