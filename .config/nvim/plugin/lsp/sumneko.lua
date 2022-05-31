f5353c8e6e2a55eddd671c17e7f2613b65fd06ca
Dundar Goc <gocdundar@gmail.com>
2022-05-31 17:13:48 +0200

    nvim: remove unnecessary config from sumneko

--- .config/nvim/plugin/lsp/sumneko.lua
+++ .config/nvim/plugin/lsp/sumneko.lua
@@ -1,25 +1,9 @@
-local data_path = vim.fn.stdpath 'data'
-
--- Make runtime files discoverable to the server
-local runtime_path = vim.split(package.path, ';')
-table.insert(runtime_path, 'lua/?.lua')
-table.insert(runtime_path, 'lua/?/init.lua')
-
 require('lspconfig').sumneko_lua.setup {
-    cmd = {
-        'lua-language-server',
-        '--logpath=' .. data_path .. '/log',
-        '--metapath=' .. data_path .. '/meta',
-    },
     on_attach = require('lsp').on_attach,
-    capabilities = capabilities,
     settings = {
         Lua = {
             runtime = {
-                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
                 version = 'LuaJIT',
-                -- Setup your lua path
-                path = runtime_path,
             },
             diagnostics = {
                 -- Get the language server to recognize the `vim` global
