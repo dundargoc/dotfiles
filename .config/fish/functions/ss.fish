41b0962c7cab7aff5c0dde80108a9e8cbfcb3361
Dundar Goc <gocdundar@gmail.com>
2022-07-19 12:42:20 +0200

    fish(ss): simplify logic so it works for all forks automatically

--- .config/fish/functions/ss.fish
+++ .config/fish/functions/ss.fish
@@ -1,19 +1,9 @@
 function ss
     set repo_path (git rev-parse --show-toplevel)
+    set repo_name (basename $repo_path)
     reset
 
-    if [ $repo_path = $HOME/programs/neovim ]
-        gh repo sync dundargoc/neovim --source neovim/neovim
-    end
-
-    if [ $repo_path = $HOME/programs/vim ]
-        gh repo sync dundargoc/vim --source vim/vim
-    end
-
-    if [ $repo_path = $HOME/programs/uncrustify ]
-        gh repo sync dundargoc/uncrustify --source uncrustify/uncrustify
-    end
-
+    gh repo sync dundargoc/$repo_name
     git -C $repo_path fetch --tags
     git -C $repo_path pull --rebase --all
 end
