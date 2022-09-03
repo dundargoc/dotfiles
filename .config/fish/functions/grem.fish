5a6b5849456900b73246c464de51a682f4c86b02
dundargoc <gocdundar@gmail.com>
2022-09-03 11:30:54 +0200

    alias(grem): make grem accept any branch. Default to master branch

--- .config/fish/functions/grem.fish
+++ .config/fish/functions/grem.fish
@@ -1,3 +1,7 @@
-function grem
-    git rebase (gh-default-branch) $argv
+function grem --wraps "git switch"
+    if count $argv >/dev/null
+        git rebase -X ours $argv
+    else
+        git rebase -X ours (gh-default-branch)
+    end
 end
