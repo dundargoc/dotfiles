2476fcb8197c2aac7ed00aff3b39eba07ff92046
dundargoc <gocdundar@gmail.com>
2022-10-06 13:20:29 +0200

    build(gps): don't check for review remote

--- .config/fish/functions/gps.fish
+++ .config/fish/functions/gps.fish
@@ -1,8 +1,4 @@
 function gps
-    if git remote | grep -w review
-        git push review
-    else
-        set current_branch (git rev-parse --abbrev-ref HEAD)
-        git push --set-upstream origin "$current_branch"
-    end
+    set current_branch (git branch --show-current)
+    git push --set-upstream origin "$current_branch"
 end
