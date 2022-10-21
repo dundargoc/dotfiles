bbbb53aeb28691da20b1dc690e7d3d35133fa00e
dundargoc <gocdundar@gmail.com>
2022-10-21 13:20:00 +0200

    alias(format): remove stalker formatting

--- .config/fish/functions/format.fish
+++ .config/fish/functions/format.fish
@@ -3,8 +3,6 @@ function format
     if [ $repo_path = $HOME ]
         fish_indent --write $XDG_CONFIG_HOME/fish/**/*.fish
         stylua $HOME/.config/nvim
-    else if [ $repo_path = $HOME/programs/stalker.nvim ]
-        stylua $repo_path
     else if [ $repo_path = $HOME/programs/uncrustify ]
         cmake --build $HOME/programs/uncrustify/build --target format-sources
     end
