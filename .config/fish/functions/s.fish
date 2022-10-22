62aae8a7e0bf0b90514a5ef64da9e30c721c8cdd
dundargoc <gocdundar@gmail.com>
2022-10-22 15:51:32 +0200

    alias(s): don't reset screen

--- .config/fish/functions/s.fish
+++ .config/fish/functions/s.fish
@@ -1,7 +1,6 @@
 function s
     set repo_path (git rev-parse --show-toplevel)
     set repo_name (basename $repo_path)
-    reset
 
     gh repo sync dundargoc/$repo_name
     git -C $repo_path fetch --tags --force
