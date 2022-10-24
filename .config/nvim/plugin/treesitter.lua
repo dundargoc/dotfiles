ef190b58a262c8b11cc6b41ee237f495f0d219d7
dundargoc <gocdundar@gmail.com>
2022-10-24 16:34:20 +0200

    nvim(ts): enable vimscript and help highlighting

--- .config/nvim/plugin/treesitter.lua
+++ .config/nvim/plugin/treesitter.lua
@@ -3,6 +3,6 @@ require('nvim-treesitter.configs').setup {
     ignore_install = { 'phpdoc' },
     highlight = {
         enable = true,
-        disable = { 'vim', 'help', 'markdown' },
+        disable = { 'markdown' },
     },
 }
