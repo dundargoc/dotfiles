5181156b9b7f9c39adb477c975565be25bd90d36
dundargoc <gocdundar@gmail.com>
2022-10-27 21:23:58 +0200

    alias(ghclean): always assume origin as remote

--- .config/fish/functions/ghclean.fish
+++ .config/fish/functions/ghclean.fish
@@ -1,11 +1,9 @@
 function ghclean
     set default_branch (gh-default-branch)
-    for remote in (git remote)
-        set merged_branches (git branch --merged $remote/$default_branch | grep -v '*' | grep -v $default_branch)
+    set merged_branches (git branch --merged origin/$default_branch | grep -v '*' | grep -v $default_branch)
 
-        for branch in $merged_branches
-            gbdd (string trim $branch)
-        end
+    for branch in $merged_branches
+        gbdd (string trim $branch)
     end
 
 end
