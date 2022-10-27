139bc3b7268e3819e93255bde68ec0705d690eff
dundargoc <gocdundar@gmail.com>
2022-10-27 21:20:26 +0200

    nvim(ghclean): don't assume default branch is master

--- .config/fish/functions/ghclean.fish
+++ .config/fish/functions/ghclean.fish
@@ -1,6 +1,7 @@
 function ghclean
+    set default_branch (gh-default-branch)
     for remote in (git remote)
-        set merged_branches (git branch --merged $remote/master | grep -v '*' | grep -v (gh-default-branch))
+        set merged_branches (git branch --merged $remote/$default_branch | grep -v '*' | grep -v $default_branch)
 
         for branch in $merged_branches
             gbdd (string trim $branch)
