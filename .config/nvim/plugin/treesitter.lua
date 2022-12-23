9a268bcb03242e1747b68615db29cea605beb8d6
dundargoc <gocdundar@gmail.com>
2022-12-23 12:08:06 +0100

    nvim(ts): remove ignore_install

--- .config/nvim/plugin/treesitter.lua
+++ .config/nvim/plugin/treesitter.lua
@@ -1,6 +1,5 @@
 require('nvim-treesitter.configs').setup {
     auto_install = true,
-    ignore_install = { 'phpdoc', 'c', 'lua', 'help', 'vim' },
     highlight = {
         enable = true,
         disable = { 'markdown' },
