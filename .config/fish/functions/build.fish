5a1d0d9611d5745d00545b008ff1bc0be3485722
dundargoc <gocdundar@gmail.com>
2023-02-28 19:39:27 +0100

    alias(build): use clang++ as c++ compiler

--- .config/fish/functions/build.fish
+++ .config/fish/functions/build.fish
@@ -2,6 +2,7 @@ function build
     set neovim_path "$HOME/programs/neovim"
 
     set -x CC clang
+    set -x CXX clang++
     make -C "$neovim_path" CMAKE_INSTALL_PREFIX="$neovim_path"
 
     command cp $HOME/programs/neovim/build/compile_commands.json $HOME/programs/neovim/
