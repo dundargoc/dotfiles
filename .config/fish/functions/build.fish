f469887d7831497c6df3706f75e7f49b5f030a09
dundargoc <33953936+dundargoc@users.noreply.github.com>
2023-03-29 16:35:10 +0200

    alias(build): only use clang if it exists

--- .config/fish/functions/build.fish
+++ .config/fish/functions/build.fish
@@ -1,9 +1,10 @@
 function build
     set neovim_path "$HOME/programs/neovim"
 
-    set -x CC clang
-    set -x CXX clang++
-    make -C "$neovim_path" CMAKE_INSTALL_PREFIX="$neovim_path"
+    if type -q clang
+        set -x CC clang
+    end
+    make -j -C "$neovim_path" CMAKE_INSTALL_PREFIX="$neovim_path"
 
     command cp $HOME/programs/neovim/build/compile_commands.json $HOME/programs/neovim/
 end
