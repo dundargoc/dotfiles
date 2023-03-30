1d830917d099fd51537dc81c7439b072e8ea4e7d
dundargoc <gocdundar@gmail.com>
2023-03-30 22:40:11 +0200

    alias(upgrade): nix

--- .config/fish/functions/upgrade.fish
+++ .config/fish/functions/upgrade.fish
@@ -17,6 +17,10 @@ function upgrade
         paru -Syu --noconfirm
     end
 
+    if type -q nix
+        nix profile upgrade '.*'
+    end
+
     if type -q nvim
         nvim --headless "+Lazy! sync" +qa
     end
