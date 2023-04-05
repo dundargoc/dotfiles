95b0e4ca331e72887576989c5fbf0cc41ce4cc5e
dundargoc <gocdundar@gmail.com>
2023-04-05 19:48:42 +0200

    alias(cpcool): remove

--- .config/fish/functions/cpcool.fish
+++
@@ -1,8 +0,0 @@
-function cpcool
-    set coolpath $HOME/programs/nvim-cool
-    set pluginpath $HOME/.config/nvim/
-
-    cp $coolpath/lua/cool.lua $pluginpath/lua/cool.lua
-    cp $coolpath/plugin/cool.vim $pluginpath/plugin/cool.vim
-    sed /nvim-cool/d -i $XDG_CONFIG_HOME/nvim/plugin/packer.lua
-end
