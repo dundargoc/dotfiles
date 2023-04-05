f3c800d04efb51d56f2045e358be5aa9906cf5ea
dundargoc <gocdundar@gmail.com>
2023-04-05 19:44:07 +0200

    alias(griv): remove

--- .config/fish/functions/griv.fish
+++
@@ -1,7 +0,0 @@
-function griv
-    set default (gh-default-branch)
-    set current (git branch --show-current)
-    set ancestor (git merge-base $default $current)
-
-    git revert --no-edit $ancestor...$current $argv
-end
