f84ff26dc9ede819c009914fbdaf4bbab496c2b3
dundargoc <gocdundar@gmail.com>
2023-04-05 19:46:00 +0200

    alias(cpanticool): remove

--- .config/fish/functions/cpanticool.fish
+++
@@ -1,7 +0,0 @@
-function cpanticool
-    set coolpath $HOME/programs/nvim-cool
-    set pluginpath $HOME/.config/nvim/
-
-    cp $pluginpath/lua/cool.lua $coolpath/lua/cool.lua
-    cp $pluginpath/plugin/cool.vim $coolpath/plugin/cool.vim
-end
