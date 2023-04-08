cb947a1d8b951b9d202e57cde09b63e1c2709aa6
dundargoc <gocdundar@gmail.com>
2023-04-08 13:03:23 +0200

    alias(build): change install location to bin

--- .config/fish/functions/build.fish
+++ .config/fish/functions/build.fish
@@ -4,7 +4,7 @@ function build
     if type -q clang
         set -x CC clang
     end
-    make -j -C "$neovim_path" CMAKE_INSTALL_PREFIX="$neovim_path"
+    make -j -C "$neovim_path" CMAKE_INSTALL_PREFIX="$neovim_path"/bin
 
     command cp $HOME/programs/neovim/build/compile_commands.json $HOME/programs/neovim/
 end
