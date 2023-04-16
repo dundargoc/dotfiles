7fc1de364c92581650f31a76ed0f1ae0a2da832a
dundargoc <gocdundar@gmail.com>
2023-04-16 13:31:28 +0200

    alias(createlocal): use neodundar

--- .config/fish/functions/createlocal.fish
+++ .config/fish/functions/createlocal.fish
@@ -1,5 +1,5 @@
 function createlocal
     gps
     set repo_name (basename (git rev-parse --show-toplevel))
-    gh pr create --fill -R dundargoc/$repo_name
+    gh pr create --fill -R neodundar/$repo_name
 end
