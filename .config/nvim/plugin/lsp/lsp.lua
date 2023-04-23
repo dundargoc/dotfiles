9dfbe5bdd3077ce7604415e9b673c258afdac8f4
dundargoc <gocdundar@gmail.com>
2023-04-23 12:51:35 +0200

    nvim(tsserver): enable suggestions

--- .config/nvim/plugin/lsp/lsp.lua
+++ .config/nvim/plugin/lsp/lsp.lua
@@ -1,4 +1,4 @@
-local servers = { 'vimls', 'clangd', 'bashls', 'jsonls', 'pyright' }
+local servers = { 'vimls', 'clangd', 'bashls', 'jsonls', 'pyright', 'tsserver' }
 for _, server in ipairs(servers) do
     require('lspconfig')[server].setup({
         on_attach = require('lsp').on_attach,
--- .config/nvim/plugin/lsp/tsserver.lua
+++
@@ -1,8 +0,0 @@
-require('lspconfig').tsserver.setup({
-    on_attach = require('lsp').on_attach,
-    init_options = {
-        preferences = {
-            disableSuggestions = true,
-        },
-    },
-})
