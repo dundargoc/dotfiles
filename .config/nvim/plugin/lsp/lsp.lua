001053e18aac137f2262ca03f8873e8ebf1407d2
dundargoc <gocdundar@gmail.com>
2023-04-27 18:00:12 +0200

    nvim: remove tsserver
    It gives incorrect solutions for javascript for some reason.

--- .config/nvim/plugin/lsp/lsp.lua
+++ .config/nvim/plugin/lsp/lsp.lua
@@ -1,4 +1,4 @@
-local servers = { 'vimls', 'clangd', 'bashls', 'jsonls', 'pyright', 'tsserver', 'cmake' }
+local servers = { 'vimls', 'clangd', 'bashls', 'jsonls', 'pyright', 'cmake' }
 for _, server in ipairs(servers) do
     require('lspconfig')[server].setup({
         on_attach = require('lsp').on_attach,
