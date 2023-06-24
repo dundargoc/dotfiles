1b6becc7795193a7c4ac385b2cd5e7f85b1b5c4f
dundargoc <gocdundar@gmail.com>
2023-06-24 14:55:45 +0200

    alias(s): use pure git operations instead of gh
    gh sync has stopped working for me on multiple occasions now.

--- .config/fish/functions/s.fish
+++ .config/fish/functions/s.fish
@@ -1,8 +1,8 @@
 function s
     set repo_path (git rev-parse --show-toplevel)
     set repo_name (basename $repo_path)
+    set default_branch (gh-default-branch)
 
-    gh repo sync dundargoc/$repo_name
-    git -C $repo_path fetch --tags --force --prune --all
-    git -C $repo_path pull --rebase --all
+    git -C $repo_path fetch upstream --tags --force --prune
+    git -C $repo_path rebase upstream/$default_branch $default_branch
 end
