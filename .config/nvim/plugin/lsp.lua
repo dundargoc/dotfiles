2e8e43952f7b8df7e035a070dca72336b5793b05
dundargoc <gocdundar@gmail.com>
2023-09-13 14:23:06 +0200

    nvim(lsp): change format to space-x to prevent clashing

--- .config/nvim/plugin/lsp.lua
+++ .config/nvim/plugin/lsp.lua
@@ -77,7 +77,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
         vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
         vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-        vim.keymap.set('n', '<space>f', function()
+        vim.keymap.set('n', '<space>x', function()
             vim.lsp.buf.format { async = true }
         end, opts)
     end,
