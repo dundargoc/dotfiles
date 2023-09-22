f66da74a59d671e0432fc04dfab467deb6a1a6ea
dundargoc <gocdundar@gmail.com>
2023-09-22 11:15:48 +0200

    fish(superset): add -1 to slice to keep compatibility
    For example, fish version 3.1.2 doesn't work without the -1.

--- .config/fish/functions/superset.fish
+++ .config/fish/functions/superset.fish
@@ -1,5 +1,5 @@
 # Set variable for both global and universal scope.
 function superset
-    set -gx $argv[1] $argv[2..]
-    set -Ux $argv[1] $argv[2..]
+    set -gx $argv[1] $argv[2..-1]
+    set -Ux $argv[1] $argv[2..-1]
 end
