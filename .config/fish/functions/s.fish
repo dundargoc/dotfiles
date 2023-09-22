8bedbc13058d5e48b2bf091e64ae5582722eb7cc
dundargoc <gocdundar@gmail.com>
2023-09-22 11:14:42 +0200

    alias(s): only fetch from upstream if it exists

--- .config/fish/functions/s.fish
+++ .config/fish/functions/s.fish
@@ -3,7 +3,11 @@ function s
     set repo_name (basename $repo_path)
     set default_branch (gh-default-branch)
 
-    git -C $repo_path fetch origin --prune
-    git -C $repo_path fetch upstream --tags --force --prune
-    git -C $repo_path rebase upstream/$default_branch $default_branch
+    git -C $repo_path fetch origin --tags --force --prune
+    git -C $repo_path pull
+
+    if git ls-remote --exit-code upstream 2>/dev/null
+        git -C $repo_path fetch upstream --tags --force --prune
+        git -C $repo_path rebase upstream/$default_branch $default_branch
+    end
 end
