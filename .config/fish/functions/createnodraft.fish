7d9398d39ee2a183f8cf92e526cdfe59f7ea9539
dundargoc <gocdundar@gmail.com>
2023-09-26 16:58:40 +0200

    alias: add createnodraft
    It's like create but with no draft.

---
+++ .config/fish/functions/createnodraft.fish
@@ -0,0 +1,22 @@
+function createnodraft
+    set current_branch (git rev-parse --abbrev-ref HEAD)
+    set default_branch (gh-default-branch)
+    set repo_path (git rev-parse --show-toplevel)
+
+    if [ $repo_path = $HOME/programs/dundar-org/neovim ]
+        echo "Currently on neodundar. Abort."
+        return
+    end
+
+    if [ $repo_path = $HOME/programs/dundar-org/nvim-treesitter ]
+        echo "Currently on neodundar. Abort."
+        return
+    end
+
+    if [ $current_branch != $default_branch ]
+        gps
+        gh pr create --fill
+    else
+        echo "Current branch is master. Abort."
+    end
+end
