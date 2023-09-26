3d1969ac37d4c9281963de8e9adc6bc2832fca6e
dundargoc <gocdundar@gmail.com>
2023-09-26 21:48:33 +0200

    alias(nukenvim): remove question

--- .config/fish/functions/nukenvim.fish
+++ .config/fish/functions/nukenvim.fish
@@ -1,8 +1,3 @@
 function nukenvim
-    echo "Are you sure?"
-    read answer
-
-    if [ $answer = y ]
-        rm -rf $HOME/.config/nvim $HOME/.local/share/nvim $HOME/.local/state/nvim $HOME/.cache/nvim
-    end
+    rm -rf $HOME/.config/nvim $HOME/.local/share/nvim $HOME/.local/state/nvim $HOME/.cache/nvim
 end
