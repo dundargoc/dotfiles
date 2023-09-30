479c05e576c7ca2b0dde42a6ce14194ba37ee453
dundargoc <gocdundar@gmail.com>
2023-09-30 14:18:08 +0200

    cppcheck: add suppress list

--- .config/fish/functions/cppcheck.fish
+++ .config/fish/functions/cppcheck.fish
@@ -1,4 +1,59 @@
 function cppcheck
+    set USE_SUPPRESS_LIST true
+
+    if test "$USE_SUPPRESS_LIST" = true
+        set suppress_list \
+            --suppress=AssignmentIntegerToAddress \
+            --suppress=arrayIndexOutOfBoundsCond \
+            --suppress=assignBoolToPointer \
+            --suppress=checkLevelNormal \
+            --suppress=clarifyCalculation \
+            --suppress=compareBoolExpressionWithInt \
+            --suppress=constParameter \
+            --suppress=constParameterCallback \
+            --suppress=constParameterPointer \
+            --suppress=constParameterReference \
+            --suppress=constVariable \
+            --suppress=constVariablePointer \
+            --suppress=constVariableReference \
+            --suppress=cstyleCast \
+            --suppress=duplicateBreak \
+            --suppress=duplicateCondition \
+            --suppress=internalAstError \
+            --suppress=invalidLifetime \
+            --suppress=invalidPointerCast \
+            --suppress=invalidPrintfArgType_sint \
+            --suppress=invalidPrintfArgType_uint \
+            --suppress=knownConditionTrueFalse \
+            --suppress=legacyUninitvar \
+            --suppress=missingReturn \
+            --suppress=moduloofone \
+            --suppress=nullPointer \
+            --suppress=nullPointerRedundantCheck \
+            --suppress=objectIndex \
+            --suppress=preprocessorErrorDirective \
+            --suppress=redundantAssignment \
+            --suppress=redundantInitialization \
+            --suppress=redundantPointerOp \
+            --suppress=returnDanglingLifetime \
+            --suppress=shadowFunction \
+            --suppress=sizeofDivisionMemfunc \
+            --suppress=sizeofwithnumericparameter \
+            --suppress=syntaxError \
+            --suppress=truncLongCastAssignment \
+            --suppress=uninitMemberVar \
+            --suppress=uninitStructMember \
+            --suppress=uninitvar \
+            --suppress=unknownEvaluationOrder \
+            --suppress=unreachableCode \
+            --suppress=unreadVariable \
+            --suppress=unsignedLessThanZero \
+            --suppress=unusedStructMember \
+            --suppress=uselessAssignmentPtrArg \
+            --suppress=wrongPrintfScanfArgNum \
+            --suppress=zerodivcond
+    end
+
     set repo_path (git rev-parse --show-toplevel)
     reset
 
@@ -16,5 +71,6 @@ function cppcheck
         set flags --std=c99
     end
 
-    command cppcheck --project=compile_commands.json --enable=all --disable=unusedFunction --suppress=missingIncludeSystem --suppress=missingInclude --quiet -j$cores $flags $argv
+    command cppcheck --project=compile_commands.json --enable=all --disable=unusedFunction --suppress=missingIncludeSystem --suppress=missingInclude \
+        $suppress_list --quiet -j$cores $flags $argv
 end
