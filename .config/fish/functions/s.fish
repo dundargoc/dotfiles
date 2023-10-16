8fa23c20cdc76376ee2a42c528bcf1f75f743bb2
dundargoc <gocdundar@gmail.com>
2023-10-16 15:19:53 +0200

    alias(s): allow specifying branch

--- .config/fish/functions/s.fish
+++ .config/fish/functions/s.fish
@@ -1,13 +1,17 @@
 function s
     set repo_path (git rev-parse --show-toplevel)
     set repo_name (basename $repo_path)
-    set default_branch (gh-default-branch)
+    if count $argv >/dev/null
+        set branch $argv
+    else
+        set branch (gh-default-branch)
+    end
 
     git -C $repo_path fetch origin --tags --force --prune
     git -C $repo_path pull
 
     if git ls-remote --exit-code upstream &>/dev/null
-        git -C $repo_path fetch upstream $default_branch --tags --force --prune
-        git -C $repo_path rebase upstream/$default_branch $default_branch
+        git -C $repo_path fetch upstream $branch --tags --force --prune
+        git -C $repo_path rebase upstream/$branch $branch
     end
 end
