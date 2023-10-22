5c61cd4a35ff2c51a8b09f54d31c014c0812e71d
dundargoc <gocdundar@gmail.com>
2023-10-22 14:36:32 +0200

    nvim(ts): disable python highlighting
    It causes an indentation bug

--- .config/nvim/plugin/treesitter.lua
+++ .config/nvim/plugin/treesitter.lua
@@ -2,7 +2,7 @@ require('nvim-treesitter.configs').setup({
     auto_install = true,
     highlight = {
         enable = true,
-        disable = { 'cmake' },
+        disable = { 'cmake', 'python' },
     },
     ensure_installed = {
         "bash",
