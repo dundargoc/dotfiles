adbaace9413f0f73be1dd99acfc70ecfde6fe446
dundargoc <33953936+dundargoc@users.noreply.github.com>
2023-10-24 12:47:14 +0200

    alias(buildvim): build vim even if bear is missing

--- .config/fish/functions/buildvim.fish
+++ .config/fish/functions/buildvim.fish
@@ -3,7 +3,9 @@ function buildvim
     set vim_path "$HOME/programs/vim"
     if type -q compiledb
         compiledb -o $vim_path/compile_commands.json make -C $vim_path -j
-    else
+    else if type -q bear
         bear --append --output $vim_path/compile_commands.json -- make -C $vim_path -j
+    else
+        make -C $vim_path -j
     end
 end
