5b1d6cfaa61a0b6af462f72a58a46b75f0854b31
dundargoc <33953936+dundargoc@users.noreply.github.com>
2023-11-03 10:45:44 +0100

    misc(jenkins): allow dumping credentials from directory

--- .misc/jenkins/jenkins-dump-credentials.groovy
+++ .misc/jenkins/jenkins-dump-credentials.groovy
@@ -1,10 +1,22 @@
-import java.nio.charset.StandardCharsets;
-def creds = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
-      com.cloudbees.plugins.credentials.Credentials.class
-)
+import com.cloudbees.plugins.credentials.*
+import com.cloudbees.plugins.credentials.common.*
+import com.cloudbees.plugins.credentials.domains.*
+import com.cloudbees.plugins.credentials.impl.*
+import com.cloudbees.jenkins.plugins.sshcredentials.impl.*
+import org.jenkinsci.plugins.plaincredentials.*
+
+def item = Jenkins.instance
+//def item = Jenkins.instance.getItem("myFolderName"); // replace with item to get folder or item scoped credentials
+
+def creds = CredentialsProvider.lookupCredentials(
+        com.cloudbees.plugins.credentials.Credentials.class,
+        item,
+        null,
+        null
+);
 
 for (c in creds) {
-  c.properties.each{k,v ->
+    c.properties.each{k,v ->
     if(k != 'class' && k!= 'descriptor'){
         println(k + ": " + v)
     }
