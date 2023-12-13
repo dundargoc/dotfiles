2a7a4f339eb1081e14605983537a270d45488673
dundargoc <gocdundar@gmail.com>
2023-12-13 19:48:28 +0100

    nvim: use mini.files

--- .config/nvim/plugin/mini.lua
+++ .config/nvim/plugin/mini.lua
@@ -7,3 +7,5 @@ require('mini.operators').setup({
     sort = { prefix = 'gs', },
 })
 require('mini.splitjoin').setup()
+require('mini.files').setup({ content = { prefix = function() end } })
+vim.keymap.set({ 'n', 'x' }, '<leader>n', function() MiniFiles.open() end)
