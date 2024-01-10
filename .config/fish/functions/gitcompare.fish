9cd5c5da400fd9f2ae1571c073232ad2176627a7
dundargoc <gocdundar@gmail.com>
2024-01-10 23:27:12 +0100

    alias(gitcompare): remove graph and add topo-order

--- .config/fish/functions/gitcompare.fish
+++ .config/fish/functions/gitcompare.fish
@@ -1,4 +1,4 @@
 function gitcompare
     set default_branch (gh-default-branch)
-    git log --right-only --graph --cherry-pick --oneline HEAD...$default_branch $argv
+    git log --right-only --topo-order --cherry-pick --oneline HEAD...$default_branch $argv
 end
