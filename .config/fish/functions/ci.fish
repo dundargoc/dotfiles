959304bdc2e3331e258b838102923f8455499df8
dundargoc <gocdundar@gmail.com>
2024-04-11 13:27:23 +0200

    alias(ci): don't add files

--- .config/fish/functions/ci.fish
+++ .config/fish/functions/ci.fish
@@ -1,6 +1,4 @@
 function ci
     set repo_path (git rev-parse --show-toplevel)
-    rm -f $repo_path/.cirrus.yml $repo_path/.github/workflows/{api-docs.yml,build.yml,codeql.yml,test.yml}
-    git add -A
-    git commit -m "fixup: TESTING - REMOVE COMMIT LATER"
+    rm -f $repo_path/.cirrus.yml $repo_path/.github/workflows/{api-docs.yml,build.yml,codeql.yml,test.yml,test_windows.yml}
 end
