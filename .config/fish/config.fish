a1fd7246ca1b517f0360056582d8f8a8b38ecd27
dundargoc <gocdundar@gmail.com>
2024-05-31 13:11:27 +0200

    fish: add $HOME/.luarocks to PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,6 +10,7 @@ end
 fish_add_path $HOME/.bin
 fish_add_path $HOME/.bin/*/bin
 fish_add_path $HOME/.cargo/bin
+fish_add_path $HOME/.luarocks/bin
 fish_add_path $HOME/.local/bin
 
 fish_add_path /home/linuxbrew/.linuxbrew/bin
