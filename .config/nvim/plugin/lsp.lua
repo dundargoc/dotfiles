0fa6643fdbb41b318733a7658dd5b22a3d47426d
dundargoc <gocdundar@gmail.com>
2024-06-07 12:31:34 +0200

    nvim: remove [d and ]d mappings
    They're provided by default now.

--- .config/nvim/plugin/lsp.lua
+++ .config/nvim/plugin/lsp.lua
@@ -41,8 +41,6 @@ lspconfig.lua_ls.setup({
 
 -- Global mappings.
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 
 -- Use LspAttach autocommand to only map the following keys
