f44c41fbc7279a7edb59fe95618ce67fde3328a5
dundargoc <gocdundar@gmail.com>
2024-11-26 15:06:40 +0100

    nvim: disable make ts

--- .config/nvim/plugin/treesitter.lua
+++ .config/nvim/plugin/treesitter.lua
@@ -2,7 +2,7 @@ require('nvim-treesitter.configs').setup({
     auto_install = true,
     highlight = {
         enable = true,
-        disable = { 'python' },
+        disable = { 'python', 'make' },
     },
     ensure_installed = {
         "bash",
