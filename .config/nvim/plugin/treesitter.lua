cb26723f6a9b1f05ae7437500d6cfdb6c72f3150
dundargoc <gocdundar@gmail.com>
2024-12-15 20:27:58 +0100

    nvim(ts): add note about python

--- .config/nvim/plugin/treesitter.lua
+++ .config/nvim/plugin/treesitter.lua
@@ -1,5 +1,7 @@
 require('nvim-treesitter.configs').setup({
     auto_install = true,
+    -- NOTE: python indenting seems wack if I only enable TS highlighing. It
+    -- might get fixed if I also use the TS indentation alongside it.
     highlight = {
         enable = true,
         disable = { 'python', 'make' },
