d932722d8531ecfcc651887daade194e103a36c7
dundargoc <gocdundar@gmail.com>
2025-02-22 13:48:56 +0100

    alias(bi): add --clean flag

--- .config/fish/functions/bi.fish
+++ .config/fish/functions/bi.fish
@@ -1,4 +1,4 @@
 function bi
     build
-    VIMRUNTIME="$NVIM/runtime" $NVIM/build/bin/nvim -c "set runtimepath^=$NVIM/build/runtime" --luamod-dev $argv
+    VIMRUNTIME="$NVIM/runtime" $NVIM/build/bin/nvim --clean -c "set runtimepath^=$NVIM/build/runtime" --luamod-dev $argv
 end
