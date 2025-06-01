f3ae4ccd50998bc0fdfd038235b1a015a01cc158
dundargoc <gocdundar@gmail.com>
2025-06-01 16:09:47 +0200

    nvim: disable automatic_enable from mason-lspconfig

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -27,6 +27,7 @@ require('lazy').setup({
     {
         'williamboman/mason-lspconfig.nvim',
         opts = {
+            automatic_enable = false,
             ensure_installed = {
                 "bashls", "clangd", "jsonls", "lua_ls", "pyright", "rust_analyzer", "ts_ls", "yamlls", "zls"
             },
