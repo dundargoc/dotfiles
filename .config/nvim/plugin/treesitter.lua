41304cd25f7be36b8b736e25b5c25548e9c982ae
dundargoc <gocdundar@gmail.com>
2026-04-15 10:16:58 +0200

    nvim(treesitter): start treesitter

--- .config/nvim/plugin/treesitter.lua
+++ .config/nvim/plugin/treesitter.lua
@@ -1,8 +1,29 @@
 require('nvim-treesitter').install {
-        "bash",
-        "fish",
-        "javascript",
-        "nix",
-        "python",
-        "yaml",
+    "bash",
+    "fish",
+    "git_config",
+    "git_rebase",
+    "gitcommit",
+    "javascript",
+    "json",
+    "kitty",
+    "nix",
+    "python",
+    "requirements",
+    "starlark",
+    "toml",
+    "yaml",
+    "zsh",
 }
+
+vim.api.nvim_create_autocmd('FileType', {
+    callback = function()
+        pcall(function() vim.treesitter.start() end)
+        -- vim.treesitter.start()
+    end,
+})
+
+-- vim.api.nvim_create_autocmd('FileType', {
+--     pattern = { '*' },
+--     callback = function() vim.treesitter.start() end,
+-- })
