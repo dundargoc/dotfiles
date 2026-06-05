c9ccb22214936595f782138245772bf879c51381
dgoc <dundar.goc@volvocars.com>
2026-06-05 10:04:13 +0200

    nvim: include neotree

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -56,6 +56,18 @@ require('lazy').setup({
     'neovim/nvim-lspconfig',
     'dundargoc/nvim-cool',
     {
+        {
+            "nvim-neo-tree/neo-tree.nvim",
+            branch = "v3.x",
+            dependencies = {
+                "nvim-lua/plenary.nvim",
+                "MunifTanjim/nui.nvim",
+                "nvim-tree/nvim-web-devicons", -- optional, but recommended
+            },
+            lazy = false,            -- neo-tree will lazily load itself
+        }
+    },
+    {
         -- There is already a built-in replacement for this, see
         -- `:h terminal-scrollback-pager`
         'powerman/vim-plugin-AnsiEsc',
