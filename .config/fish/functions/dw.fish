2d93ed0823f3f1947b631e78e29a704c027d813d
dundargoc <gocdundar@gmail.com>
2026-08-25 10:26:28 +0200

    alias(dw): remove

--- .config/fish/functions/dw.fish
+++
@@ -1,4 +0,0 @@
-function dw
-    set gradle_version (get_gradle_version)
-    d wrapper --gradle-distribution-url=https://esw-artifactory.got.volvo.net/artifactory/gradle-distributions/gradle-$gradle_version-bin.zip
-end
