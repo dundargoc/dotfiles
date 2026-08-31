db56a3b770d74862bf5f3e954b7a0d6bb3282eb8
dgoc <dundar.goc@volvocars.com>
2026-08-31 15:02:45 +0200

    alias(gitcompare): allow specifying a specific file

--- .config/fish/functions/gitcompare.fish
+++ .config/fish/functions/gitcompare.fish
@@ -1,8 +1,8 @@
 function gitcompare --wraps "git switch"
     if count $argv >/dev/null
-        set branch $argv
+        set branch $argv[1]
     else
         set branch (gh-default-branch)
     end
-    git log --right-only --topo-order --cherry-pick --oneline --no-merges HEAD...$branch $argv
+    git log --right-only --topo-order --cherry-pick --oneline --no-merges HEAD...$branch $argv[2..]
 end
