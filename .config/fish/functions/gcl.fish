b1204636b92f1e442bf99062e9325b604ccd6ea3
dgoc <dundar.goc@volvocars.com>
2026-09-08 10:46:24 +0200

    alias(gcl): when cloning from gerrit, apply the commit-msg automatically
    Since gerrit requires the "Change-Id" string in commit message to work,
    it makes sense to just always add the commit message hook that adds it.

--- .config/fish/functions/gcl.fish
+++ .config/fish/functions/gcl.fish
@@ -1,6 +1,14 @@
 function gcl
     if string match -r -q -- "^(ssh|https)://" $argv
         git clone $argv
+        if string match -r -q -- "^ssh://gerrit" $argv
+            set name (string split "/" $argv)[-1]
+            cd $name
+            set git_path (git rev-parse --git-dir)
+            mkdir -p $git_path/hooks/
+            curl -fLo $git_path/hooks/commit-msg https://gerrit.neo.volvocars.net/tools/hooks/commit-msg
+            chmod +x $git_path/hooks/commit-msg
+        end
     else
         gh repo clone $argv[1] -- $argv[2..]
     end
